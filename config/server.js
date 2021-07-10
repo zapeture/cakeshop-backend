b57d1d380092ad13ede35b1d805d5710e8b84d82
Fortune Zviregei <64714576+zapeture@users.noreply.github.com>
2021-07-10 13:06:28 +0200

    first commit v1.0.0

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2ed86df09d6ac748489012235d783518'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b4b2093c-a2f3-45d3-a0bc-d45394a854da'
+};
